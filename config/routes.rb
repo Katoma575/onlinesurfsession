79bb760a3abf8f54714e349206dee8be8d3e0a96
EC2 Default User <ec2-user@ip-172-31-34-124.ap-northeast-1.compute.internal>
2020-11-05 23:29:23 +0000

    changed from post to get for users/guest_sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  root 'homes#top'
+  
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
@@ -8,10 +10,9 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "sign_in", :to => "users/sessions#new"
     get '/users/sign_out' => 'devise/sessions#destroy'
-    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+    get 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-
-  root 'homes#top'
+  
 
   get '/users/sign_out' => 'devise/sessions#destroy'
   get 'homes/greeting'=>'homes#greeting'
