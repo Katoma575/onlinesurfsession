b2abaddc2f3e28d781e7d721a741268f5c883e2c
EC2 Default User <ec2-user@ip-172-31-34-124.ap-northeast-1.compute.internal>
2020-11-06 07:26:25 +0000

    changed devise_for user to users

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   
   root 'homes#index'
   
-  devise_for :user, controllers: {
-    registrations: 'users/registrations'
-  }
+  devise_for :users
   
   devise_scope :user do
     post '/users/guest_sign_in', to: 'users/sessions#new_guest'
