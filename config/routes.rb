85128115829e615d12ede5923ba3d511a482185a
EC2 Default User <ec2-user@ip-172-31-34-124.ap-northeast-1.compute.internal>
2020-11-06 02:02:10 +0000

    changed easy login code to symple one

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,13 @@ Rails.application.routes.draw do
   
   root 'homes#index'
   
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
-  }
-  
-  devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
-    get '/users/sign_out' => 'devise/sessions#destroy'
-    get 'users/guest_sign_in', to: 'users/sessions#new_guest'
-  end
+  devise_for :users
+  post '/homes/guest_sign_in', to: 'homes#new_guest'
   
   get 'homes/greeting'=>'homes#greeting'
   get 'homes/how_to'=>'homes#how_to'
   get 'contacts/new'=>'contacts#new'
   
- 
   resource :user, except: [:new, :create,]
   resources :contacts, only: [:new, :create ]
   
